4be5e2faaa8cbbc35f3af130e5b4af20aea85cf8
0svaldo-Chavz <os_antony99@hotmail.com>
2023-03-09 11:48:08 -0600

    nuevo

--- djangoCRUD/settings.py
+++ djangoCRUD/settings.py
@@ -81,7 +81,10 @@ WSGI_APPLICATION = 'djangoCRUD.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost/postgres',
+        conn_max_age=600
+    )
 }
 
 
